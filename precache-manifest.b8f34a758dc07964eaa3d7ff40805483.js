6548ab88ecf41381d60456b889a30c51e182dbf1
Muhammad Ibrahim <mohammadibrahim.ibrahim9@gmail.com>
2019-11-04 19:02:43 +0500

    Updates

---
+++ precache-manifest.b8f34a758dc07964eaa3d7ff40805483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0e4e64029edf8f6afadc4e0014279d",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "adeba7ea806f0550b854",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e23d694074480f838a1d",
+    "url": "/RoboFriends/static/css/main.67a1c2cc.chunk.css"
+  },
+  {
+    "revision": "adeba7ea806f0550b854",
+    "url": "/RoboFriends/static/js/2.f554a252.chunk.js"
+  },
+  {
+    "revision": "e23d694074480f838a1d",
+    "url": "/RoboFriends/static/js/main.ad546638.chunk.js"
+  },
+  {
+    "revision": "66b61e323bab0ea024e3",
+    "url": "/RoboFriends/static/js/runtime-main.11357dbf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
